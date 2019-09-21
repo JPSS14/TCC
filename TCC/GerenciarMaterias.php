8a6fa63c952cae05cc6b2557926aa5242c57c43d
JPSS14 <jp_scarabelli@hotmail.com>
2019-09-21 17:19:40 -0300

    UPDATE 4: Adicionar Matérias

---
+++ TCC/GerenciarMaterias.php
@@ -0,0 +1,38 @@
+<?php
+
+include ("Classes/Classes.php");
+include ("Classes/Conexão.php");
+
+?>
+
+<?php
+    session_start();
+    if (!isset($_SESSION["cpf"])){
+    header("location:Login.php");
+    }else $cpfS = $_SESSION["cpf"];
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Saira+Stencil+One&display=swap" rel="stylesheet"> 
+        <meta charset="utf-8">
+        <script type="text/javascript" src="javaScript.js"></script>
+        <title>TCC</title>
+    </head>
+    
+    <body>
+        
+        <header>
+            <h1>CYBER PROVAS</h1>
+        </header>
+          
+        <a href="AdicionarMaterias.php">Adicionar Matérias</a>
+        
+    </body>
+
+</html>
--- TCC/IndexAdm.php
+++ TCC/IndexAdm.php
@@ -18,6 +18,7 @@
         <a href="Alterar.php">Alterar meu usuario</a><br>
         <a href="AdicionarAdm.php">Adicionar novo Adm</a><br>
         <a href="DeletarUsuario.php">Deletar Usuário</a><br>
+        <a href="GerenciarMaterias.php">Gerenciar Matérias</a><br>
         <p><a href="Logout.php">Sair</a></p>
     
     </body>
