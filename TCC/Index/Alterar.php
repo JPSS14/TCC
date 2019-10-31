a3c93b805fdc836842787254d9a6a636dd9f57d4
JPSS14 <jp_scarabelli@hotmail.com>
2019-10-31 19:04:21 -0300

    UPDATE 21: Correções de bugs

--- TCC/Index/Alterar.php
+++ TCC/Index/Alterar.php
@@ -44,9 +44,10 @@
                 $cx = $c->conexão(); 
                 $pro = new Professor();
                 $prof = $pro->exibirDadosProfessor($cx, $cpfS);
-                $linha = mysqli_fetch_assoc($prof)
+                $linha = mysqli_fetch_assoc($prof);
+            echo $linha["email"];
             ?>
-        <div> 
+      </div>
 	    <nav class="navbar navbar-expand-lg navbar-light"  style="background-color:#048abf">
             <a class="navbar-brand" href="../Validacao.php"style="margin-left:45%">
                 <img src="logoipp.png" width="110" height="auto" alt="">
@@ -134,6 +135,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+            
     </body>
 </html> 
 
